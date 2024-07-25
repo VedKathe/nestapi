cd6aef89e6df2c76171518880e0942a9fa43ea64
Vedkathe <vmkathe@gmail.com>
2024-07-25 19:11:53 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,19 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://example.com',
+      'http://www.example.com',
+      'http://app.example.com',
+      'https://example.com',
+      'https://www.example.com',
+      'https://app.example.com',
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
+  });
   app.use(cookieParser());
   await app.listen(3000);
 }
